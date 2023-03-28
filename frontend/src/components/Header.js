fdd89ac6f281fd55e82e403f8c09446668164bf0
Alois Napitalai <anapitalai@gmail.com>
2023-03-29 09:36:17 +1000

    jobs dependency

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>Napitalai Actions actions--Alois Napitalai- Github Actions</Navbar.Brand>
+            <Navbar.Brand>Napitalai actions--Alois Napitalai- Github Actions</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
