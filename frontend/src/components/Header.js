6d9383d428b7c3fb4ecca8464688771f64f77898
Alois Napitalai <anapitalai@gmail.com>
2023-03-28 11:25:02 +1000

    Setup actions

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>RalikuShop - Github Actions</Navbar.Brand>
+            <Navbar.Brand>Napitalai Actions actions- Github Actions</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
