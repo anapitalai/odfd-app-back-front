f726070de8af91988accdb99685ef2eeaae086bb
Alois Napitalai <anapitalai@gmail.com>
2023-03-29 12:57:06 +1000

    Issued

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>PNG ON DEMAND</Navbar.Brand>
+            <Navbar.Brand>PNG ON DEMAND is online now</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
