feea56d1a144bccc79027d3455c3d019ca29dad8
Alois Napitalai <anapitalai@gmail.com>
2023-03-29 11:07:43 +1000

    working I guess

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy;Alois </Col>
+          <Col className='text-center py-3'>Copyright &copy;Alois XXXCXCCCCCC</Col>
         </Row>
       </Container>
     </footer>
