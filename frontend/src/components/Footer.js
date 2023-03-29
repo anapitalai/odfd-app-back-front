1bd698e8f6979faea9e6af4d9c64f127d7d82f35
Alois Napitalai <anapitalai@gmail.com>
2023-03-29 10:43:42 +1000

    actionss

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy;Alois</Col>
+          <Col className='text-center py-3'>Copyright &copy;Alois XX</Col>
         </Row>
       </Container>
     </footer>
