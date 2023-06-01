def270a728f6cc6005626fff838e6c38823de4cc
Alois Napitalai <anapitalai@gmail.com>
2023-06-01 17:21:24 +1000

    Add db credentials to env file

--- py_code/app.py
+++ py_code/app.py
@@ -6,15 +6,22 @@ from flask_pymongo import PyMongo,ObjectId
 from flask_cors import CORS
 import pandas as pd
 import json
+from dotenv import load_dotenv
+load_dotenv()
+secret_key = os.environ.get("SECRET_KEY")
+user=os.environ.get("DB_USER")
+pw=os.environ.get("DB_PW")
+db=os.environ.get("DB_NAME")
+
 
 
 
 # Init app
 app = Flask(__name__)
 
-app.secret_key= 'acb5efac7654a9f927bf5206a8b52bad5e6eb52d'
-##app.config['MONGO_URI']="mongodb://202.1.39.189/odfd"
-app.config['MONGO_URI']="mongodb+srv://odfd_user:odfdAt2023@nictc.ok4ic.mongodb.net/odfd?retryWrites=true&w=majority"
+app.secret_key= secret_key
+##app.config['MONGO_URI']="mongodb://192.1.39.189/odfd"
+app.config['MONGO_URI']="mongodb+srv://"+user+":"+pw+"@nictc.ok4ic.mongodb.net/"+db+"?retryWrites=true&w=majority"
 
 # Set the Stable API version when creating a new client
 client = PyMongo(app)
